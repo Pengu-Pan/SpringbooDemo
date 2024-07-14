b1b5b1ae25d4d86a3c169be56f596f2bb84a99ba
Pengu <18774831033@163.com>
2024-07-15 00:14:02 +0800

    fix：配置中心

--- order-service/src/main/java/example/OrderApplication.java
+++ order-service/src/main/java/example/OrderApplication.java
@@ -1,5 +1,6 @@
 package example;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@EnableApolloConfig
 public class OrderApplication {
     public static void main(String[] args) {
 
